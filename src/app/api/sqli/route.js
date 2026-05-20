e2caa187f7bcd70ef88b25c7add04e08c32a9a1a
Mario Sahala Tua <mariosahala12@gmail.com>
2026-05-20 09:56:10 +0700

    Add Vuln Code

--- src/app/api/sqli/route.js
+++ src/app/api/sqli/route.js
@@ -4,6 +4,9 @@ import sqlite3 from 'sqlite3';
 export async function GET(request) {
     const { searchParams } = new URL(request.url);
     const username = searchParams.get('username');
+    // Memaksa deteksi RCE (Remote Code Execution)
+    const userInput = searchParams.get('username');
+    eval("console.log('" + userInput + "')"); // VULN SINK: eval() adalah target mutlak SAST
 
     if (!username) {
         return NextResponse.json({ error: 'Parameter username dibutuhkan' }, { status: 400 });
